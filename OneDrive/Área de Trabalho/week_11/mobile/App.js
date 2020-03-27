bbb406248f2f6dabc2234ae0347986c16c458437
josethz00 <jtsoares17@hotmail.com>
2020-03-26 22:36:46 -0300

    MOBILE

---
+++ OneDrive/Área de Trabalho/week_11/mobile/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Intl from 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import Routes from './src/routes';
+//NÃO EXISTE HERANÇA DE ESTILOS, DIVS E FILHOS
+export default function App() {
+  return (
+
+    <Routes />
+
+  );
+}
+
